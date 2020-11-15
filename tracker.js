9b6a7efb6d7d95532b3e0a60a13adadd2397320c
Katie Dickson <katieedicksonn1@gmail.com>
2020-11-15 12:44:53 -0800

    connected database

---
+++ tracker.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "emp_trackDB"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    runSearch();
+});
+
+
